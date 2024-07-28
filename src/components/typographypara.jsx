5d49c88f51fed6037c81bc86c3ec95299024d09d
Jay Suthar <suthajay3635@gmail.com>
2024-07-28 13:41:40 +0530

    typo

--- src/components/typographypara.jsx
+++ src/components/typographypara.jsx
@@ -4,7 +4,7 @@ export function P({ className, children, style }) {
   return (
     <p
       className={twMerge("leading-7 [&:not(:first-child)]:mt-4", className)}
-      style={style}
+      style={{ fontFamily: "PolySansSlim", ...style }}
     >
       {children}
     </p>
